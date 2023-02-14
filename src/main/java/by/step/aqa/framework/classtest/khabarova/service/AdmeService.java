7999a9c1db50f0fbb679b559ef395278b6e7c176
Olya <olga.khabarova2013@yandex.ru>
2023-02-14 12:43:41 +0300

    added new file

--- src/main/java/by/step/aqa/framework/classtest/khabarova/service/AdmeService.java
+++ src/main/java/by/step/aqa/framework/classtest/khabarova/service/AdmeService.java
@@ -3,6 +3,9 @@ package by.step.aqa.framework.classtest.khabarova.service;
 import by.step.aqa.framework.classtest.khabarova.page.AdmePage;
 import org.openqa.selenium.WebDriver;
 
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 public class AdmeService extends BaseService{
     AdmePage admePage = new AdmePage(driver);
     public AdmeService (WebDriver driver){
@@ -10,6 +13,8 @@ public class AdmeService extends BaseService{
     }
 
     public AdmePage getAdmePage() {
+        Logger log = Logger.getLogger(AdmeService.class.getName());
+        log.log(Level.WARNING, "Find error");
         return admePage;
     }
 }
--- src/main/java/by/step/aqa/framework/classtest/khabarova/service/PikabuService.java
+++ src/main/java/by/step/aqa/framework/classtest/khabarova/service/PikabuService.java
@@ -1,8 +1,11 @@
 package by.step.aqa.framework.classtest.khabarova.service;
 
 import by.step.aqa.framework.classtest.khabarova.page.PikabuPage;
-import com.fasterxml.jackson.databind.ser.Serializers;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.openqa.selenium.WebDriver;
+
+
 
 public class PikabuService extends BaseService{
     private PikabuPage pikabuPage = new PikabuPage(driver);
@@ -11,6 +14,8 @@ public class PikabuService extends BaseService{
         super(driver);
     }
     public PikabuPage getPikabuPage(){
+        Logger log = LoggerFactory.getLogger(PikabuService.class);
+        log.trace( "New error find");
         return pikabuPage;
     }
 
